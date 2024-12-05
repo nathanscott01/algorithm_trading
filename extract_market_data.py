12309983914ba6207ef565a769cc3f2bbd283459
Nathan Scott <scotty05nj@gmail.com>
2024-12-05 22:29:50 +1300

    Need to implement discarding a data entry if not complete

--- extract_market_data.py
+++ extract_market_data.py
@@ -25,21 +25,82 @@ def fetch_snp500_stocklist():
     return updated_sp500_tickers
 
 
+# def fetch_snp500_data(tickers):
+#     """Fetch stock market data for the stocks in tickers"""
+#     my_columns = ['Stock', 'Stock Price', 'Previous Close', 'Overnight Price Return', 'Market Capitalisation',
+#                   'Trailing Price to Earnings Ratio', 'Number of Shares to Buy']
+#     data = []
+#     for ticker in tickers:
+#         try:
+#             stock = yf.Ticker(ticker)
+#             info = stock.info
+#             overnight_price_change = info.get('currentPrice') / info.get('previousClose')
+#             data.append([ticker, info.get('currentPrice'), info.get('previousClose'), overnight_price_change,
+#                          info.get('marketCap'),
+#                          info.get('trailingPE'), 'N/A'])
+#         except Exception as e:
+#             print(f"Error fetching data for {ticker}: {e}")
+#     final_dataframe = pd.DataFrame(data, columns=my_columns)
+#     return final_dataframe
+
+
+def calculate_price_returns(stock_info):
+    """Calculate the price return percentage for 12, 6, 3 and 1 months"""
+    # historic_price = stock_info.history(period="1y")
+    # # print(historic_price)
+    # if len(historic_price) < 252:
+    #     print("Not enough data")
+    #     return False
+    try:
+        historic_price = stock_info.history(period="1y")
+        today_price = stock_info.info.get('currentPrice')
+        one_month_price = historic_price['Close'].iloc[-22]
+        three_month_price = historic_price['Close'].iloc[-66]
+        six_month_price = historic_price['Close'].iloc[-126]
+        twelve_month_price = historic_price['Close'].iloc[0]
+
+        one_month_return = (today_price - one_month_price) / one_month_price
+        three_month_return = (today_price - three_month_price) / three_month_price
+        six_month_return = (today_price - six_month_price) / six_month_price
+        twelve_month_return = (today_price - twelve_month_price) / twelve_month_price
+
+        results = [twelve_month_return, six_month_return, three_month_return, one_month_return]
+    except:
+        results = False
+
+    return results
+
+
 def fetch_snp500_data(tickers):
     """Fetch stock market data for the stocks in tickers"""
-    my_columns = ['Stock', 'Stock Price', 'Previous Close', 'Overnight Price Return', 'Market Capitalisation',
-                  'Trailing Price to Earnings Ratio', 'Number of Shares to Buy']
+    my_columns = ['Stock',
+                  'Stock Price',
+                  'Market Capitalisation',
+                  'Number of Shares to Buy',
+                  'One-Year Price Return',
+                  'One-Year Return Percentile',
+                  'Six-Month Price Return',
+                  'Six-Month Return Percentile',
+                  'Three-Month Price Return',
+                  'Three-Month Return Percentile',
+                  'One-Month Price Return',
+                  'One-Month Return Percentile',
+                  'HQM Score',
+                  'Trailing Price to Earnings Ratio']
     data = []
     for ticker in tickers:
         try:
             stock = yf.Ticker(ticker)
             info = stock.info
-            overnight_price_change = info.get('currentPrice') / info.get('previousClose')
-            data.append([ticker, info.get('currentPrice'), info.get('previousClose'), overnight_price_change,
-                         info.get('marketCap'),
-                         info.get('trailingPE'), 'N/A'])
+            price_returns = calculate_price_returns(stock)
+            if price_returns:
+                data.append([ticker, info.get('currentPrice'), info.get('marketCap'), 'N/A',
+                             price_returns[0], 'N/A', price_returns[1], 'N/A',
+                             price_returns[2], 'N/A', price_returns[3], 'N/A',
+                             'N/A', info.get('trailingPE')])
         except Exception as e:
             print(f"Error fetching data for {ticker}: {e}")
+
     final_dataframe = pd.DataFrame(data, columns=my_columns)
     return final_dataframe
 
--- quantitative_momentum.py
+++ quantitative_momentum.py
@@ -11,6 +11,7 @@ import pandas as pd
 import xlsxwriter
 import math
 from scipy import stats
+from statistics import mean
 from extract_market_data import extract_market_data
 
 
@@ -23,10 +24,36 @@ def retrieve_data():
         extract_market_data()
 
     data = pd.read_csv(filename)
-    data.sort_values('Overnight Price Return', ascending=False, inplace=True)
-    data = data[:51]
-    data.reset_index(drop=True, inplace=True)
     return data
+
+
+def calc_momentum_percentile(data):
+    """Calculate the momentum percentiles for the given data"""
+    time_periods = [
+        "One-Year",
+        "Six-Month",
+        "Three-Month",
+        "One-Month"
+    ]
+
+    for row in data.index:
+        for time_period in time_periods:
+            data.loc[row, f'{time_period} Return Percentile'] = (
+                    stats.percentileofscore(
+                        data[f'{time_period} Price Return'],
+                        data.loc[row, f'{time_period} Price Return'])/100)
+
+    for time_period in time_periods:
+        print(data[f'{time_period} Return Percentile'])
+
+    for row in data.index:
+        momentum_percentiles = []
+        for time_period in time_periods:
+            momentum_percentiles.append(data.loc[row, f'{time_period} Return Percentile'])
+        data.loc[row, 'HQM Score'] = mean(momentum_percentiles)
+
+    data.sort_values(by="HQM Score", ascending=False)
+    return data[:51]
 
 
 def shares_to_buy(stock_dataframe):
@@ -56,7 +83,7 @@ def shares_to_buy(stock_dataframe):
 def build_spreadsheet(final_data):
     """Write our final data to a csv file"""
     writer = pd.ExcelWriter('quantitative_momentum_recommended_trades.xlsx', engine='xlsxwriter')
-    final_data.to_excel(writer, sheet_name='Recommended Trades', index=False)
+    final_data.to_excel(writer, sheet_name='Momentum Strategy', index=False)
 
     background_color = '#ffffff'
     font_color = '#000000'
@@ -84,22 +111,40 @@ def build_spreadsheet(final_data):
             'border': 1
         }
     )
+    percent_format = writer.book.add_format(
+        {
+            'num_format': '0.0%',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
     column_formats = {
         'A': ['Stock', string_format],
         'B': ['Stock Price', dollar_format],
-        'C': ['Previous Close', dollar_format],
-        'D': ['Overnight Price Return', integer_format],
-        'E': ['Market Capitalisation', dollar_format],
-        'F': ['Number of Shares to Buy', integer_format]
+        'C': ['Market Capitalisation', dollar_format],
+        'D': ['Number of Shares to Buy', integer_format],
+        'E': ['One-Year Price Return', percent_format],
+        'F': ['One-Year Return Percentile', percent_format],
+        'G': ['Six-Month Price Return', percent_format],
+        'H': ['Six-Month Return Percentile', percent_format],
+        'I': ['Three-Month Price Return', percent_format],
+        'J': ['Three-Month Return Percentile', percent_format],
+        'K': ['One-Month Price Return', percent_format],
+        'L': ['One-Month Return Percentile', percent_format],
+        'M': ['HQM Score', integer_format],
+        'N': ['Trailing Price to Earnings Ratio', dollar_format]
     }
 
     for column in column_formats.keys():
-        writer.sheets['Recommended Trades'].set_column(f'{column}:{column}', 20, column_formats[column][1])
-        writer.sheets['Recommended Trades'].write(f'{column}1', column_formats[column][0], string_format)
+        writer.sheets['Momentum Strategy'].set_column(f'{column}:{column}', 20, column_formats[column][1])
+        writer.sheets['Momentum Strategy'].write(f'{column}1', column_formats[column][0], string_format)
 
     writer.close()
 
 
 stock_data = retrieve_data()
-final_stock_data = shares_to_buy(stock_data)
+momentum_percentile_data = calc_momentum_percentile(stock_data)
+final_stock_data = shares_to_buy(momentum_percentile_data)
 build_spreadsheet(final_stock_data)
